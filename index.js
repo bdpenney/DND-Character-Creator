ffe6e4a4862371912e5349673ab04d2a4ff7adba
Becca Penney <70587957+bdpenney@users.noreply.github.com>
2021-01-27 11:12:02 -0330

    HTML/CSS/JS
    Toggle hide/show elements

---
+++ index.js
@@ -0,0 +1,19 @@
+function classFunction() {
+    document.getElementById("class").style.display = 'block';
+  }
+
+  function raceFunction() {
+    document.getElementById("race").style.display = 'block';
+  }
+
+  function abilityFunction() {
+    document.getElementById("abilityscores").style.display = 'block';
+  }
+
+  function heroFunction() {
+      document.getElementById("hero").style.display= 'none';
+  }
+
+  function heroFunction() {
+      document.getElementById("hero").style.display= 'block';
+  }
