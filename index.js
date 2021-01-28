86722c5ef1ac765446adef5a6987f15a37397e03
Becca Penney <70587957+bdpenney@users.noreply.github.com>
2021-01-27 20:57:59 -0330

    HTML/CSS
    Added hide/show to appropriate buttons

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 function classFunction() {
     document.getElementById("class").style.display = 'block';
+    document.getElementById("hero").style.display= 'none';
+    document.getElementById("navbar").style.display= 'block';
+  }
+
+  function class2Function() {
+      document.getElementById("class").style.display= 'block';
+      document.getElementById("race").style.display= 'none';
   }
 
   function raceFunction() {
     document.getElementById("race").style.display = 'block';
+    document.getElementById("class").style.display= 'none';
+    document.getElementById("abiliyscores").style.display= 'none';
   }
 
   function abilityFunction() {
     document.getElementById("abilityscores").style.display = 'block';
-  }
-
-  function heroFunction() {
-      document.getElementById("hero").style.display= 'none';
+    document.getElementById("race").style.display= 'none';
   }
 
   function heroFunction() {
       document.getElementById("hero").style.display= 'block';
+      document.getElementById("class").style.display= 'none';
+      document.getElementById("navbar").style.display= 'none';
+  }
+
+  function CSFunction() {
+      document.getElementById("charactersheet").style.display= 'block';
   }
