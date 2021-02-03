b4d3ab4bb46bd3a5414d1b2b3314a2abdd9daca2
Becca Penney <70587957+bdpenney@users.noreply.github.com>
2021-02-02 23:07:47 -0330

    JS
    Button functionality through Event Listeners

--- index.js
+++ index.js
@@ -1,5 +1,96 @@
-document.getElementById("myBtn").addEventListener("click", );
+/* Const NavBar Buttons */
+const homeButton = document.getElementById("homeButton");
+const classButton = document.getElementById("classButton");
+const raceButton = document.getElementById("raceButton");
+const abilitiesButton = document.getElementById("abilitiesButton");
+const charactersheetButton = document.getElementById("charactersheetButton");
 
-function displayDate() {
-  document.getElementById("demo").innerHTML = Date();
-}
+/* Const Back/Continue Buttons */
+const heroButton = document.getElementById("heroButton");
+const heroBack = document.getElementById("heroBack")
+const raceContinue = document.getElementById("raceContinue");
+const classBack = document.getElementById("classBack")
+const abilitiesContinue = document.getElementById("abilitiesContinue")
+const raceBack = document.getElementById("raceBack");
+const charactersheetContinue = document.getElementById("charactersheetContinue");
+
+/* Const Sections */
+const navbarSection = document.getElementById("navbarSection");
+const heroSection = document.getElementById("heroSection");
+const classSection = document.getElementById("classSection");
+const raceSection = document.getElementById("raceSection");
+const abilitiesSection = document.getElementById("abilitiesSection");
+const charactersheetSection = document.getElementById("charactersheetSection");
+
+/* Const Class Options */
+const rogueButton = 
+
+homeButton.addEventListener('click', () => {
+    heroSection.classList.remove('hidden');
+    navbarSection.classList.add('hidden');
+})
+
+classButton.addEventListener('click', () => {
+    classSection.classList.remove('hidden');
+    raceSection.classList.add('hidden');
+    abilitiesSection.classList.add('hidden');
+    charactersheetSection.classList.add('hidden');
+})
+
+raceButton.addEventListener('click', () => {
+    raceSection.classList.remove('hidden');
+    classSection.classList.add('hidden');
+    abilitiesSection.classList.add('hidden');
+    charactersheetSection.classList.add('hidden');
+})
+
+abilitiesButton.addEventListener('click', () => {
+    abilitiesSection.classList.remove('hidden');
+    raceSection.classList.add('hidden');
+    classSection.classList.add('hidden');
+    charactersheetSection.classList.add('hidden');
+})
+
+charactersheetButton.addEventListener('click', () => {
+    charactersheetSection.classList.remove('hidden');
+    raceSection.classList.add('hidden');
+    abilitiesSection.classList.add('hidden');
+    classSection.classList.add('hidden');
+})
+
+heroButton.addEventListener('click', () => {
+    classSection.classList.remove('hidden');
+    heroSection.classList.add('hidden');
+    navbarSection.classList.remove('hidden');
+})
+
+heroBack.addEventListener('click', () => {
+    heroSection.classList.remove('hidden');
+    classSection.classList.add('hidden');
+    navbarSection.classList.add('hidden');
+})
+
+raceContinue.addEventListener('click', () => {
+    raceSection.classList.remove('hidden');
+    classSection.classList.add('hidden');
+})
+
+classBack.addEventListener('click', () => {
+    classSection.classList.remove('hidden');
+    raceSection.classList.add('hidden');
+})
+
+abilitiesContinue.addEventListener('click', () => {
+    abilitiesSection.classList.remove('hidden');
+    raceSection.classList.add('hidden');
+})
+
+raceBack.addEventListener('click', () => {
+    raceSection.classList.remove('hidden');
+    abilitiesSection.classList.add('hidden');
+})
+
+charactersheetContinue.addEventListener('click', () => {
+    charactersheetSection.classList.remove('hidden');
+    abilitiesSection.classList.add('hidden');
+})
