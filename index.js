e05283795bbbd4e5191707922eed283e449511bd
Becca Penney <70587957+bdpenney@users.noreply.github.com>
2021-03-08 19:24:27 -0330

    Comments
    Comments for changes that need to be made

--- index.js
+++ index.js
@@ -38,31 +38,10 @@ const rogueButton = document.getElementById("rogueButton");
 const fighterButton = document.getElementById("fighterButton");
 const wizardButton = document.getElementById("wizardButton");
 
-/* Abilities Selection Consts */
-
-const strLeft = document.getElementById("strLeft");
-const strRight = document.getElementById("strRight");
-
-let strResult = 8;
-
-strLeft.addEventListener('click', () => {
-    if(strResult > 8 ){
-        strResult --;
-        document.getElementById("strResult").innerHTML = strResult;
-      }
-})
-
-strRight.addEventListener('click', () => {
-    if(strResult < 13 ){
-        strResult ++; 
-        document.getElementById("strResult").innerHTML = strResult;
-      }
-})
-
-
-
-
 /* Class Selection Event Listeners */
+
+
+//Add a way to only add 'classSelected' to the chosen class//
 
 rogueButton.addEventListener('click', () => {
     rogueButton.classList.add('classSelected');
@@ -95,6 +74,91 @@ gnomeButton.addEventListener('click', () => {
     gnomeButton.classList.add('classSelected');
     characterSelections[1] = 'Gnome';
 })
+
+/* Abilities Selection Consts */
+//Find a way to simplify this code//
+
+const strLeft = document.getElementById("strLeft");
+const strRight = document.getElementById("strRight");
+
+let strResult = 8;
+
+strLeft.addEventListener('click', () => {
+    if(strResult > 8 ){
+        strResult --;
+        document.getElementById("strResult").innerHTML = strResult;
+      }
+})
+
+strRight.addEventListener('click', () => {
+    if(strResult < 13 ){
+        strResult ++; 
+        document.getElementById("strResult").innerHTML = strResult;
+      }
+})
+
+//Search for global options for these buttons// 
+
+const dexLeft = document.getElementById("dexLeft");
+const dexRight = document.getElementById("dexRight");
+
+let dexResult = 8;
+
+dexLeft.addEventListener('click', () => {
+    if(dexResult > 8 ){
+        dexResult --;
+        document.getElementById("dexResult").innerHTML = dexResult;
+      }
+})
+
+dexRight.addEventListener('click', () => {
+    if(dexResult < 13 ){
+        dexResult ++; 
+        document.getElementById("dexResult").innerHTML = dexResult;
+      }
+})
+
+const conLeft = document.getElementById("conLeft");
+const conRight = document.getElementById("conRight");
+
+let conResult = 8;
+
+//Find a way to disable 'click' when ability= 8//
+conLeft.addEventListener('click', () => {
+    if(conResult > 8 ){
+        conResult --;
+        document.getElementById("conResult").innerHTML = conResult;
+      }
+})
+
+//Add a way to increase by 2 when increasing value from 13 >> 14 and 14 >> 15//
+conRight.addEventListener('click', () => {
+    if(conResult < 13 ){
+        conResult ++; 
+        document.getElementById("conResult").innerHTML = conResult;
+      }
+})
+
+const intLeft = document.getElementById("intLeft");
+const intRight = document.getElementById("intRight");
+
+//Keep variablr for ability value or write in HTML?//
+let intResult = 8;
+
+intLeft.addEventListener('click', () => {
+    if(intResult > 8 ){
+        intResult --;
+        document.getElementById("intResult").innerHTML = intResult;
+      }
+})
+
+intRight.addEventListener('click', () => {
+    if(intResult < 13 ){
+        intResult ++; 
+        document.getElementById("intResult").innerHTML = intResult;
+      }
+})
+
 
 /* Abilities Selection Event Listeners */
 
